c6f8312e2b87567ad5fecfb1e0a368cb57153512
Dilan <diporrasc@unal.edu.co>
2024-06-09 16:09:14 -0500

    Add usage with args

--- fraction.cpp
+++ fraction.cpp
@@ -27,6 +27,15 @@ std::pair<int, int> fraction(float n, double precision = 0.0000001)
 
 int main(int argc, char const *argv[])
 {
+    if (argc != 3 && argc != 1){
+        std::cout << "Usage: " << argv[0] << " [number] [precision]" << std::endl;
+        return 1;
+    }else if (argc == 3){
+        std::pair<int,int> result = fraction(std::stod(argv[1]), std::stod(argv[2]));
+        std::cout << result.first << " / " << result.second << std::endl;
+        std::cout << std::setprecision(20) << static_cast<double>(result.first) / result.second << std::endl;
+        return 0;
+    }
     
     std::pair<int,int> result = fraction(3.14159265, 0.00000001);
     std::cout << result.first << " / " << result.second << std::endl;
